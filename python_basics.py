d9d01e22e1f7c7950aef9da254438512d79662e2
Gianna Cosenza <giacosey28@gmail.com>
2026-03-12 15:22:35 -0400

    Printing basic data types and variables

--- python_basics.py
+++ python_basics.py
@@ -1,3 +1,14 @@
-age = 25 
+#String and Integer
+age = 19
+name = "Gianna"
+print("Hello, World!", "My name is", name, "and my age is", age)
 
-print(age)
+
+#Float
+price = 9.99
+print("$", price)
+
+
+#Boolean
+is_logged_in = True
+is_admin = False
